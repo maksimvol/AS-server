e3ad56633c4fbf55ba3b2a5b64a4acee319a5635
maksimvol <108937188+maksimvol@users.noreply.github.com>
2024-05-17 16:56:10 +0300

    Port change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
@@ -10,6 +11,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors());
 
 // Games Router
 const gamesRouter = require('./routes/games')
@@ -24,4 +26,4 @@ app.use('/jackpots', jackpotsRouter)
 const mathsRouter = require('./routes/maths')
 app.use('/maths', mathsRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(3001, () => console.log('Server Started'))
